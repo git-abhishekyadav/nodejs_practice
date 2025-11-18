4480a524b3f9201aea24622257d8eba92134f076
git-abhishekyadav <abhishekyadav28@outlook.com>
2025-11-18 22:15:55 +0530

    event emitter and express server

---
+++ express-server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res) => {
+    res.send("Hello WOrld");
+});
+
+app.use((err,req,res,next) => {
+    if(err) {
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Listening to SErver on port ${PORT}`);
+});
